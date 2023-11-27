72d3332819e2381ad6989b3a955f0f88135349c2
mahmud666999 <mahmud.hassan@g.bracu.ac.bd>
2023-11-27 21:51:26 +0600

    It has working mvc redirection and login page

---
+++ src/main/java/com/example/springjwt/SpringjwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.springjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringjwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringjwtApplication.class, args);
+    }
+
+}
