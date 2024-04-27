75b1f92f49f3876042c00deee5fc8a7e251db596
mahmud666999 <112693092+mahmud666999@users.noreply.github.com>
2024-04-27 18:05:12 +0600

    Update SecurityCustomization.java

--- src/main/java/com/example/springjwt/SecurityCustomization.java
+++ src/main/java/com/example/springjwt/SecurityCustomization.java
@@ -18,7 +18,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.bind.annotation.CrossOrigin;
-
+//will add jwt soon
 @Configuration
 @EnableWebSecurity(debug = true)
 @AllArgsConstructor
